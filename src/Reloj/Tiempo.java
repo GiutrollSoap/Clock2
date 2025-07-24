b1e7b1aea97a60776041bdeca5d618ccf4b00e62
SoapGiutroll <Giutroll117troll@gmail.com>
2025-07-24 15:47:18 -0600

    Se crea Tiempo.java
    Se creo tiempo

---
+++ src/Reloj/Tiempo.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reloj;
+
+/**
+ *
+ * @author UTN
+ */
+public abstract class Tiempo {
+    private int hora;
+    private int minuto;
+    private int segundo;
+    
+    
+    public Tiempo(){
+        this.hora=0;
+        this.minuto=0;
+        this.segundo=0;
+    }
+    
+    public void correr(){
+        segundo++;
+        if (segundo==60){
+            segundo=0;
+            minuto++;
+        }
+         
+        if (minuto==60){
+            minuto=0;
+            hora++;
+        }
+           
+        if (hora==24){
+            hora=0;
+            
+        }
+    }
+    
+    public String toString(){
+        String h,m,s;
+         h=String.valueOf(hora);
+        if(hora<10){
+            h="0".concat(h); 
+        }
+             m=String.valueOf(minuto);
+        if(minuto<10){
+            m="0".concat(m); 
+        }
+             s=String.valueOf(segundo);
+        if(segundo<10){
+            s="0".concat(s); 
+        }
+        return h+":"+m+":"+s;
+    }
+}
