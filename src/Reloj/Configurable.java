7d174997a891dd47854d855ee18d945611b57b4a
dylpb <dylanpabri@gmail.com>
2025-07-24 16:11:57 -0600

    Se crea la interfaz configurable
    Se definen sus atributos

---
+++ src/Reloj/Configurable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reloj;
+
+/**
+ *
+ * @author Dylan
+ */
+public interface Configurable {
+    public void configurar(int h,int m,int s);
+    
+}
